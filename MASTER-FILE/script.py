7027682463f308326a40c2d90221ab401252689e
Davide Arcolini <davide.arcolini@studenti.polito.it>
2022-04-12 21:10:44 +0200

    someone help me with the script lol

--- MASTER-FILE/script.py
+++ MASTER-FILE/script.py
@@ -1,2 +1,22 @@
 import os
 
+PATH = "./GUI_images/"
+
+def parse_string(filename: str) -> str:
+	filename_list = list(filename)
+	for i in range(len(filename_list)):
+		if filename_list[i] == " ":
+			filename_list[i] = "-"
+	return ''.join(filename_list)
+
+# Listing subtree
+entries = os.listdir(PATH) 
+for directory in entries:
+	path_tmp = PATH + directory + "/"
+	images = os.listdir(path_tmp)
+	for item in images:
+		if item != ".DS_Store":
+			item = parse_string(item)
+			print(f"![{item}]({path_tmp}{item})")
+
+
