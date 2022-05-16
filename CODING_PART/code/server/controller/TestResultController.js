87aab44f96179456c1a8844e6e1f7afaa2d9ce17
RiccardoStud <100519209+RiccardoStud@users.noreply.github.com>
2022-05-16 10:51:19 +0200

    Sistemato TestResult controller

--- CODING_PART/code/server/controller/TestResultController.js
+++ CODING_PART/code/server/controller/TestResultController.js
@@ -10,12 +10,9 @@ class TestResultController {
     
     getTestResults = async (req, res) =>{
 
-        //search on DB
+        //Search on DB
         const sql = "SELECT * FROM TEST_RESULTS WHERE rfid==?";
         const result = await this.daotr.all(sql,req.params.rfid);
-
-        //controllo da cancellare, ma guarda 
-        console.log(result.length);
         
         return res.status(200).json(result);
     }
@@ -24,7 +21,7 @@ class TestResultController {
 
     getTestResultById = async (req, res) => {
 
-        //control id, rfid by function test (already in js)
+        //Validation
         if(/^[0-9]+$/.test(req.params.id)===false || /^[0-9]+$/.test(req.params.rfid)===false){
             return res.status(422).json();
         }
@@ -34,21 +31,19 @@ class TestResultController {
         const testarray = await this.daotr.all(sql,req.params.rfid);
 
         //RFID doesn't exist
-        if(testarray==null){
+        if(testarray[0]==undefined){
             return res.status(404).json();
         }
 
-        //search the ID
+        //Search the ID
         const result=testarray.filter((test)=>(test.id==req.params.id));
 
         //ID doesn't exist
-        if(result==null){
+        if(result[0]==undefined){
             return res.status(404).json();
         }
 
-        //create and deliver json file 
-        //result = {id: objectfinded.id, name: objectfinded.name, procedureDescription: objectfinded.procedureDescription, idSKU: objectfinded.idSKU };
-        console.log("RESULT : "+ JSON.stringify(result))
+        //Deliver json file 
         return res.status(200).json(result);
     }
 
@@ -56,24 +51,32 @@ class TestResultController {
 
     createTestResult = async (req, res) => {
         
-        //control the validation of the input
-        if ( req.body.idTestDescriptor===undefined) {
+        //Validation
+        if ( /^[0-9]+$/.test(req.body.idTestDescriptor)===false || /^[0-9]+$/.test(req.body.rfid)===false  ) {
             return res.status(422).json();           
           }
         
-        //aggiungere se rfid or idtest non esiste-> errore 404
+        //Control of RFID
+        let sql ="SELECT * FROM SKUITEMS WHERE RFID==?"
+        let result = await this.daotr.all(sql,req.body.rfid);
 
+        //RFID doesn't exist
+        if(result[0]==undefined){
+            return res.status(404).json();
+        }
 
-        //creation id
-        let sql = "SELECT MAX(id) as id FROM TEST_RESULTS WHERE rfid==?"
-        const max_id = await this.daotr.get(sql,req.body.rfid); 
-        let id=1;
-        if(max_id.id!==null)
-            id = max_id.id+1;
+        //Control of idtest
+        sql ="SELECT * FROM TEST_DESCRIPTORS WHERE id==?"
+        result = await this.daotr.all(sql,req.body.idTestDescriptor);
 
-        //database immission
-        sql = "INSERT INTO TEST_RESULTS(id, rfid, idTestDescriptor, Date, Result.) VALUES(?,?,?,?,?)";
-        await this.daotr.run(sql,[id, req.body.rfid, req.body.idTestDescriptor, req.body.Date, req.body.Result]);
+        //idTest doesn't exist
+        if(result[0]==undefined){
+            return res.status(404).json();
+        }
+
+        //Database immission
+        sql = "INSERT INTO TEST_RESULTS(rfid, idTestDescriptor, Date, Result) VALUES(?,?,?,?)";
+        await this.daotr.run(sql,[req.body.rfid, req.body.idTestDescriptor, req.body.Date, req.body.Result]);
 
         return res.status(201).json();
 
@@ -83,32 +86,49 @@ class TestResultController {
 
     modifyTestResult = async(req,res) => {
 
-        //newIdtestDescriptor, newDate and newResult
-        //control id by function test (already in js)
-        if(/^[0-9]+$/.test(req.params.id)===false ||/^[0-9]+$/.test(req.params.rfid)===false|| req.body.newIdtestDescriptor===undefined || req.body.newDate===undefined || req.body.newResult===undefined){
+        //Validation
+        if(/^[0-9]+$/.test(req.params.id)===false ||/^[0-9]+$/.test(req.params.rfid)===false|| req.body.newIdTestDescriptor===undefined || req.body.newDate===undefined || req.body.newResult===undefined){
             return res.status(422).json();
         }
-        
-        //Find the RFID
-        let sql = "SELECT * FROM TEST_RESULTS WHERE rfid==? "
-        const testarray = await this.daotr.all(sql,req.params.rfid);
 
-        //RFID doesn't exist
-        if(testarray==null){
+        //Control of RFID
+        let sql ="SELECT * FROM SKUITEMS WHERE RFID==?"
+        let result = await this.daotr.all(sql,req.body.rfid);
+
+        //RFID doesn't exist (it isn't created)
+        if(result[0]==undefined){
             return res.status(404).json();
         }
 
-        //search the ID
-        let result=testarray.filter((test)=>(test.id==req.params.id));
+        //Control of idtest
+        sql ="SELECT * FROM TEST_DESCRIPTORS WHERE id==?"
+        result = await this.daotr.all(sql,req.body.newIdTestDescriptor);
+
+        //idTest doesn't exist
+        if(result[0]==undefined){
+            return res.status(404).json();
+        }
+ 
+        //Find the RFID
+        sql = "SELECT * FROM TEST_RESULTS WHERE rfid==? "
+        const testarray = await this.daotr.all(sql,req.params.rfid);
+
+        //RFID doesn't exist (don't have a test result)
+        if(testarray[0]==undefined){
+            return res.status(404).json();
+        }
+
+        //Search the ID
+        result=testarray.filter((test)=>(test.id==req.params.id));
 
         //ID doesn't exist
-        if(result==null){
+        if(result[0]==undefined){
             return res.status(404).json();
         }
 
         //Update the object if found
         sql = "UPDATE TEST_RESULTS SET IdtestDescriptor=?, Date=?, Result=?  WHERE id==?";
-        result = await this.daotr.run(sql,[req.body.newIdtestDescriptor, req.body.newDate, req.body.newResult, req.params.id]);
+        result = await this.daotr.run(sql,[req.body.newIdTestDescriptor, req.body.newDate, req.body.newResult, req.params.id]);
         return res.status(200).json();
 }
 
@@ -116,7 +136,7 @@ class TestResultController {
 
     deleteTestResult = async (req, res) => {
 
-        //control id by function test (already in js)
+        //Validation
         if(/^[0-9]+$/.test(req.params.id)===false||/^[0-9]+$/.test(req.params.rfid)===false){
             return res.status(422).json();
         }
@@ -126,7 +146,7 @@ class TestResultController {
         const testarray = await this.daotr.all(sql,req.params.rfid);
 
         //RFID doesn't exist
-        if(testarray==null){
+        if(testarray[0]==undefined){
             return res.status(404).json();
         }
 
@@ -134,7 +154,7 @@ class TestResultController {
         let result=testarray.filter((test)=>(test.id==req.params.id));
 
         //ID doesn't exist
-        if(result==null){
+        if(result[0]==undefined){
             return res.status(404).json();
         }
 
