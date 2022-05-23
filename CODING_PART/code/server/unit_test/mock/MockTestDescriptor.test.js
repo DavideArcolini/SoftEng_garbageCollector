03fedaf1425f32d9a115f592c11889c144d57ab3
RiccardoStud <100519209+RiccardoStud@users.noreply.github.com>
2022-05-23 17:04:48 +0200

    Add files via upload

---
+++ CODING_PART/code/server/unit_test/mock/MockTestDescriptor.test.js
@@ -0,0 +1,216 @@
+const dao = require("./mockDB/mock_dao");
+const TestDescriptorController = require("../controller/TestDescriptorController");
+const td = new TestDescriptorController(dao);
+
+
+/*
+                                ======================================================
+                               ||                    TESTING CASES                   ||
+                                ======================================================
+*/
+
+
+/*
+                                ------------------------------------------------------
+                                |          GET  /api/testDescriptors                 |
+                                ------------------------------------------------------
+*/
+
+
+//200
+getTestDescriptors([{
+    id: 1,
+    name: "test descriptor 1",
+    procedureDescription: "This test is described by...",
+    idSKU: 1
+}], [{
+    id: 1,
+    name: "test descriptor 1",
+    procedureDescription: "This test is described by...",
+    idSKU: 1
+}]);
+
+//500? throw error?
+getTestDescriptors([], 500);
+
+
+/*
+                                ------------------------------------------------------
+                                |          GET  /api/testDescriptors/:id             |
+                                ------------------------------------------------------
+*/
+
+
+//200
+getTestDescriptorById({id: 1}, {
+    id: 1,
+    name: "test descriptor 1",
+    procedureDescription: "This test is described by...",
+    idSKU: 1
+},{
+    id: 1,
+    name: "test descriptor 1",
+    procedureDescription: "This test is described by...",
+    idSKU: 1
+});
+
+//404
+getTestDescriptorById({id: 1}, undefined, 404);
+
+//500
+getTestDescriptorById(undefined, undefined, 500);
+
+/*
+                                ------------------------------------------------------
+                                |             POST  /api/testDescriptor              |
+                                ------------------------------------------------------
+*/
+
+
+//201
+createTestDescriptor(1, 201, 201, {
+    name: "test descriptor 3",
+    procedureDescription: "This test is described by...",
+    idSKU: 1
+})
+
+//404
+createTestDescriptor(undefined, 201, 404, {
+    name: "test descriptor 3",
+    procedureDescription: "This test is described by...",
+    idSKU: 1
+})
+
+//503
+createTestDescriptor(undefined, 201, 503, undefined)
+
+/*
+                                ------------------------------------------------------
+                                |            PUT  /api/testDescriptor/:id            |
+                                ------------------------------------------------------
+*/
+
+
+//200
+modyfyTestDescriptor([1,1], 200, 200, {
+    newName: "test descriptor 1",
+    newProcedureDescription: "This test is described by...",
+    newIdSKU :1
+}, {id: 1})
+
+//404 SKUID doesn't exist
+modyfyTestDescriptor([undefined,1], 200, 404, {
+    newName: "test descriptor 1",
+    newProcedureDescription: "This test is described by...",
+    newIdSKU :1
+}, {id: 1})
+
+//404 ID doesn't exist
+modyfyTestDescriptor([1,undefined], 200, 404, {
+    newName: "test descriptor 1",
+    newProcedureDescription: "This test is described by...",
+    newIdSKU :1
+}, {id: 1})
+
+//503
+modyfyTestDescriptor([1,1], 200, 503, undefined, {id: 1})
+
+
+/*
+                                ------------------------------------------------------
+                                |            DELETE  /api/testDescriptor/:id         |
+                                ------------------------------------------------------
+*/
+
+
+//204
+deleteTestDescriptor(1,204,204,{id: 1})
+
+//404
+deleteTestDescriptor(undefined,204,404,{id: 1})
+
+//503
+deleteTestDescriptor(1,204,503,undefined)
+
+
+/*
+                                ======================================================================
+                               ||        DEFINITIONS OF THE TESTING FUNCTIONS TEST DESCRIPTOR        ||
+                                ======================================================================
+*/
+
+function getTestDescriptorById(id,resultget,expected){
+    describe('get test by id', ()=> { 
+        beforeEach(() => {
+            dao.get.mockReset();
+            dao.get.mockReturnValue(resultget)
+        })
+    
+        test('get test by id', async () => {
+            let res = await td.getTestDescriptorById(id);
+            expect(res).toEqual(expected)
+        })
+    })
+}
+
+function getTestDescriptors(resultget,expected){
+    describe('get tests', ()=> { 
+        beforeEach(() => {
+            dao.all.mockReset();
+            dao.all.mockReturnValue(resultget)
+        })
+    
+        test('get tests', async () => {
+            let res = await td.getTestDescriptors();
+            expect(res).toEqual(expected)
+        })
+    })
+}
+
+function createTestDescriptor(resultget,resultrun,expected,json){
+    describe('add test', ()=> { 
+    beforeEach(() => {
+        dao.get.mockReset();
+        dao.get.mockReturnValueOnce(resultget);
+        dao.run.mockReset();
+        dao.run.mockReturnValueOnce(resultrun);
+    })
+
+    test('add test', async () => {
+        let res = await td.createTestDescriptor(json);
+        expect(res).toEqual(expected)
+    })
+})}
+
+function modyfyTestDescriptor(resultget,resultrun,expected,json,id){
+    describe('edit test', ()=> { 
+        beforeEach(() => {
+            dao.get.mockReset();
+            dao.get.mockReturnValueOnce(resultget[0]).mockReturnValue(resultget[1])
+            dao.run.mockReset();
+            dao.run.mockReturnValueOnce(resultrun)
+        })
+    
+        test('edit test', async () => {
+            let res = await td.modifyTestDescriptor(json,id);
+            expect(res).toEqual(expected)
+        })
+    })
+}
+
+function deleteTestDescriptor(resultget,resultrun,expected,id){
+    describe('delete test', ()=> { //
+        beforeEach(() => {
+            dao.get.mockReset();
+            dao.get.mockReturnValueOnce(resultget)
+            dao.run.mockReset();
+            dao.run.mockReturnValueOnce(resultrun)
+        })
+    
+        test('delete test', async () => {
+            let res = await td.deleteTestDescriptor(id);
+            expect(res).toEqual(expected)
+        })
+    })
+    
+}
---
+++ CODING_PART/code/server/unit_test/mock/MockTestResult.test.js
@@ -0,0 +1,310 @@
+const dao = require("./mockDB/mock_dao");
+const TestResultController = require("../controller/TestResultController");
+const tr = new TestResultController(dao);
+
+
+
+/*
+                                ======================================================
+                               ||                    TESTING CASES                   ||
+                                ======================================================
+*/
+
+
+/*
+                                ------------------------------------------------------
+                                |        GET  /api/skuitems/:rfid/testResults        |
+                                ------------------------------------------------------
+*/
+
+
+//200
+getTestResults(1,[{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+}],[{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+}],{rfid: "1"})
+
+//404 Result doesn't exist
+getTestResults(undefined,[], 404, {rfid: "1"})
+
+//404 Result doesn't exist
+getTestResults(1,[], 404, {rfid: "1"})
+
+//500
+getTestResults(1,1, 500, undefined)
+
+
+/*
+                                ------------------------------------------------------
+                                |      GET  /api/skuitems/:rfid/testResults/:id      |
+                                ------------------------------------------------------
+*/
+
+
+//200
+getTestResultById([1],{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+},{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+},{rfid: "1", id: 1})
+
+//404 rfid doesn't exist
+getTestResultById([],{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+},404,{rfid: "1", id: 1})
+
+//404 id doesn't exist
+getTestResultById(1,{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+},404, {rfid: "1", id: 0})
+
+//500
+getTestResultById(undefined,undefined,500,undefined)
+
+/*
+                                ------------------------------------------------------
+                                |            POST  /api/skuitems/testResult          |
+                                ------------------------------------------------------
+*/
+
+
+//201
+createTestResult([1,1],201,201,{
+    rfid: "12345678901234567890123456789016",
+    idTestDescriptor: 12,
+    Date: "2021/11/28",
+    Result: true
+})
+
+//404 RFID
+createTestResult([undefined,1],201,404,{
+    rfid: "12345678901234567890123456789016",
+    idTestDescriptor: 12,
+    Date: "2021/11/28",
+    Result: true
+})
+
+//404 idTestDescriptor
+createTestResult([1,undefined],201,404,{
+    rfid: "12345678901234567890123456789016",
+    idTestDescriptor: 12,
+    Date: "2021/11/28",
+    Result: true
+})
+
+//503
+createTestResult([1,1],201,503,undefined)
+
+
+/*
+                                ------------------------------------------------------
+                                |        PUT  api/skuitems/:rfid/testResult/:id      |
+                                ------------------------------------------------------
+*/
+
+//200
+modifyTestResult([1,1],[{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+}],200,200,{id: 1, rfid: "1"},{
+    newIdTestDescriptor:12,
+    newDate:"2021/11/28",
+    newResult: true
+})
+
+//404 RFID doesn't exist
+modifyTestResult([undefined,1],[{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+}],200,404,{id: 1, rfid: "1"},{
+    newIdTestDescriptor:12,
+    newDate:"2021/11/28",
+    newResult: true
+})
+
+//404 idTestDescriptor doesn't exist
+modifyTestResult([1,undefined],[{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+}],200,404,{id: 1, rfid: "1"},{
+    newIdTestDescriptor:12,
+    newDate:"2021/11/28",
+    newResult: true
+})
+
+//404 RFID doesn't have test result
+modifyTestResult([1,1],[],200,404,{id: 1, rfid: "1"},{
+    newIdTestDescriptor:12,
+    newDate:"2021/11/28",
+    newResult: true
+})
+
+//404 ID TestResult doesn't exist
+modifyTestResult([1,1],[{ 
+    id: 0,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+}],200,404,{id: 1, rfid: "1"},{
+    newIdTestDescriptor:12,
+    newDate:"2021/11/28",
+    newResult: true
+})
+
+//503
+modifyTestResult([1,1],[{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+}],200,503,undefined,{
+    newIdTestDescriptor:12,
+    newDate:"2021/11/28",
+    newResult: true
+})
+
+/*
+                                ------------------------------------------------------
+                                |     DELETE  /api/skuitems/:rfid/testResult/:id     |
+                                ------------------------------------------------------
+*/
+
+
+//204
+deleteTestResult([{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+}],204,204,{id: 1, rfid: "1"})
+
+//404 RFID doesn't exist
+deleteTestResult([],204,404,{id: 1, rfid: "1"})
+
+//404 ID doesn't exist
+deleteTestResult([],204,404,{id: 0, rfid: "1"})
+
+//503
+deleteTestResult([{
+    id: 1,
+    idTestDescriptor: 14,
+    Date: "2021/11/29",
+    Result: false
+}],204,503,undefined)
+
+/*
+                                ======================================================================
+                               ||        DEFINITIONS OF THE TESTING FUNCTIONS TEST DESCRIPTOR        ||
+                                ======================================================================
+*/
+
+
+
+function getTestResults(resultget,resultall,expected,rfid) {
+    describe('get tests', ()=> { 
+        beforeEach(() => {
+            dao.all.mockReset();
+            dao.all.mockReturnValue(resultall)
+            dao.get.mockReset();
+            dao.get.mockReturnValue(resultget)
+        })
+    
+        test('get tests', async () => {
+            let res = await tr.getTestResults(rfid);
+            expect(res).toEqual(expected)
+        })
+    })
+}
+
+function getTestResultById(resultall,resultget,expected,req){
+    describe('get test by id', ()=> { 
+        beforeEach(() => {
+            dao.all.mockReset();
+            dao.all.mockReturnValue(resultall)
+            dao.get.mockReset();
+            dao.get.mockReturnValue(resultget)
+        })
+    
+        test('get test by id', async () => {
+            let res = await tr.getTestResultById(req);
+            expect(res).toEqual(expected)
+        })
+    })
+}
+
+
+function createTestResult(resultget,resultrun,expected,req){
+    describe('add test', ()=> {
+        beforeEach(() => {
+            dao.get.mockReset();
+            dao.get.mockReturnValueOnce(resultget[0]).mockReturnValue(resultget[1])
+            dao.run.mockReset();
+            dao.run.mockReturnValueOnce(resultrun);
+        })
+    
+        test('add test', async () => {
+            let res = await tr.createTestResult(req);
+            expect(res).toEqual(expected)
+        })
+    })
+}
+
+function modifyTestResult(resultget,resultall,resultrun,expected,req1,req2){
+    describe('edit test', ()=> { //
+        beforeEach(() => {
+            dao.get.mockReset();
+            dao.get.mockReturnValueOnce(resultget[0]).mockReturnValue(resultget[1])
+            dao.all.mockReset();
+            dao.all.mockReturnValue(resultall)
+            dao.run.mockReset();
+            dao.run.mockReturnValueOnce(resultrun)
+        })
+    
+        test('edit test', async () => {
+            let res = await tr.modifyTestResult(req1,req2);
+            expect(res).toEqual(expected)
+        })
+    })
+}
+
+function deleteTestResult(resultall,resultrun,expected,req){
+    describe('delete test', ()=> { //
+        beforeEach(() => {
+            dao.all.mockReset();
+            dao.all.mockReturnValue(resultall)
+            dao.run.mockReset();
+            dao.run.mockReturnValueOnce(resultrun)
+        })
+    
+        test('delete test', async () => {
+            let res = await tr.deleteTestResult(req);
+            expect(res).toEqual(expected)
+        })
+    })
+}
+
